9612703169af6b8185f721afbdc29a4c70960f7c
1Alexander1-oss <kolesnikov.1994@bk.ru>
2021-09-25 16:19:06 +0300

    added transition to two activity windows

--- app/src/main/java/com/ephemerayne/kls_notepad/MainActivity.kt
+++ app/src/main/java/com/ephemerayne/kls_notepad/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.ephemerayne.kls_notepad
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import com.ephemerayne.kls_notepad.model.NoteCreationActivity
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +15,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         addNoteButton = findViewById(R.id.add_note_button)
         addNoteButton.setOnClickListener {
-            println("click")
+            //открытие Активити
+            val intent = Intent(this, NoteCreationActivity::class.java)
+            startActivity(intent)
         }
     }
 }
---
+++ app/src/main/java/com/ephemerayne/kls_notepad/model/NoteCreationActivity.kt
@@ -0,0 +1,13 @@
+package com.ephemerayne.kls_notepad.model
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.ephemerayne.kls_notepad.R
+
+class NoteCreationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_note_creation)
+    }
+
+}
