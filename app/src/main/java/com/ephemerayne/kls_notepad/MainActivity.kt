06693644e416fdc95154f422a329b0297c7ccae5
1Alexander1-oss <kolesnikov.1994@bk.ru>
2021-09-25 15:43:34 +0300

    added FloatingActionButton its open new window to create Note

--- app/src/main/java/com/ephemerayne/kls_notepad/MainActivity.kt
+++ app/src/main/java/com/ephemerayne/kls_notepad/MainActivity.kt
@@ -2,11 +2,18 @@ package com.ephemerayne.kls_notepad
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var addNoteButton: FloatingActionButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        addNoteButton = findViewById(R.id.add_note_button)
+        addNoteButton.setOnClickListener {
+            println("click")
+        }
     }
-
 }
