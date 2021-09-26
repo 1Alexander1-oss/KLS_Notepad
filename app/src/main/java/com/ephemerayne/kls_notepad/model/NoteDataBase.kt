52152a17bc94e60ea93b3774f3e52797ebbd326a
1Alexander1-oss <kolesnikov.1994@bk.ru>
2021-09-27 01:40:09 +0300

    create new Note DataBasew

---
+++ app/src/main/java/com/ephemerayne/kls_notepad/model/NoteDataBase.kt
@@ -0,0 +1,21 @@
+package com.ephemerayne.kls_notepad.model
+
+import android.provider.BaseColumns
+
+object NoteDataBase: BaseColumns{
+    const val NOTE_NAME = "my_note"
+    const val COLUMN_NAME_TITLE = "title"
+    const val COLUMN_NAME_CONTENT = "content"
+
+    const val DATABASE_VERSION = 1
+    const val DATABASE_NAME =
+        "Note.model"
+
+    const val CREATE_NOTE = "CREATE TABLE IF NOT EXISTS ${NOTE_NAME}_NAME (" +
+
+            "${BaseColumns._ID} INTEGER PRIMARY KEY, " +
+            "$COLUMN_NAME_TITLE TEXT, " +
+            "$COLUMN_NAME_CONTENT TEXT" +
+            ")"
+    const val SQL_DELETE_TABLE = "DROP TABLE IF EXISTS ${NOTE_NAME}_NAME"
+}
