24c6c386528a36d3c7a1c1f1ab033ed81ec1a594
1Alexander1-oss <kolesnikov.1994@bk.ru>
2021-09-28 21:42:40 +0300

    complete Note DataBase

---
+++ app/src/main/java/com/ephemerayne/kls_notepad/model/NoteDataBaseHelper.kt
@@ -0,0 +1,18 @@
+package com.ephemerayne.kls_notepad.model
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class NoteDataBaseHelper(context: Context) : SQLiteOpenHelper(
+    context, NoteDataBase.DATABASE_NAME,
+    null, NoteDataBase.DATABASE_VERSION
+) {
+    override fun onCreate(p0: SQLiteDatabase?) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onUpgrade(p0: SQLiteDatabase?, p1: Int, p2: Int) {
+        TODO("Not yet implemented")
+    }
+}
