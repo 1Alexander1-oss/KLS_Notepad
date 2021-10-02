62b97db8e2ce39709b0ea77dc74e17f3ed0ca421
1Alexander1-oss <kolesnikov.1994@bk.ru>
2021-10-02 12:01:51 +0300

    create RecyclerView

---
+++ app/src/main/java/com/ephemerayne/kls_notepad/ui/NotesAdapter.kt
@@ -0,0 +1,31 @@
+package com.ephemerayne.kls_notepad.ui
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.ephemerayne.kls_notepad.R
+import com.ephemerayne.kls_notepad.model.Note
+
+class NotesAdapter : RecyclerView.Adapter<NoteViewHolder>() {
+    private val notes: ArrayList<Note> = ArrayList()
+
+    fun setNotes(notes: List<Note>) {
+        this.notes.clear()
+        this.notes.addAll(notes)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NoteViewHolder {
+        return NoteViewHolder(LayoutInflater.from(parent.context)
+            .inflate(R.layout.note_item, parent, false)) // "раздули" макет для item
+    }
+
+    override fun onBindViewHolder(holder: NoteViewHolder, position: Int) {
+        val note = notes[position]
+        holder.setContent(note) // передали обьек note в соответствующий ViewHolder
+    }
+
+    override fun getItemCount(): Int {
+       return notes.size
+    } // кол - во notes в списке
+}
