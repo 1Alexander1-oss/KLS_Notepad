99f38ae2341ab6cf3f391b6a33c7da3038eccced
1Alexander1-oss <kolesnikov.1994@bk.ru>
2021-10-02 11:28:14 +0300

    create ViewHolder for note item

--- app/src/main/java/com/ephemerayne/kls_notepad/ui/MainActivity.kt
+++ app/src/main/java/com/ephemerayne/kls_notepad/ui/MainActivity.kt
@@ -4,36 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
 import com.ephemerayne.kls_notepad.R
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     lateinit var addNoteButton: FloatingActionButton
-    lateinit var button: Button
+    lateinit var recycler: RecyclerView
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        /**
-         * это устанавливает макет. У тебя должен быть макет Менй Активити
-         * как на 17 строчке.
-         *
-         * А ты пытаешься еще поставить из другого активити макет.
-         *
-         * делай верстку. Открытие нормлаьно сделал как надо.
-         * */
-//
+        recycler = findViewById(R.id.recycler)
+
         addNoteButton = findViewById(R.id.add_note_button)
         addNoteButton.setOnClickListener {
             //открытие Активити
             val intent = Intent(this, NoteCreationActivity::class.java)
-            startActivity(intent)
-        }
-
-        button = findViewById(R.id.button)
-        button.setOnClickListener {
-            val intent = Intent(this, NoteViewActivity::class.java)
             startActivity(intent)
         }
 
---
+++ app/src/main/java/com/ephemerayne/kls_notepad/ui/NoteViewHolder.kt
@@ -0,0 +1,18 @@
+package com.ephemerayne.kls_notepad.ui
+
+import android.view.View
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.ephemerayne.kls_notepad.R
+import com.ephemerayne.kls_notepad.model.Note
+
+class NoteViewHolder(private val itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    fun setContent(note: Note) {
+        val titleView = itemView.findViewById<TextView>(R.id.title)
+        val descriptionView = itemView.findViewById<TextView>(R.id.view_description)
+        titleView.text = note.title
+        descriptionView.text = note.description // создан класс управляющий элементом списка
+        // ViewHolder
+    }
+}
