dd49d4dbeb36550c4c0d27f170ea2be94fddf66b
1Alexander1-oss <kolesnikov.1994@bk.ru>
2021-10-02 12:16:26 +0300

    get list notes

--- app/src/main/java/com/ephemerayne/kls_notepad/ui/MainActivity.kt
+++ app/src/main/java/com/ephemerayne/kls_notepad/ui/MainActivity.kt
@@ -2,21 +2,31 @@ package com.ephemerayne.kls_notepad.ui
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.ephemerayne.kls_notepad.R
+import com.ephemerayne.kls_notepad.model.Note
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
     lateinit var addNoteButton: FloatingActionButton
     lateinit var recycler: RecyclerView
-
+    private val fakeNotes: List<Note> = listOf(
+        Note(title = "посчитать бабло", description = "много бобла"),
+        Note(title = "пойти в магазин", description = "купить хлеба"),
+        Note(title = "осада", description = "соль"),
+        Note(title = "арбузы", description = "вкусные"),
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         recycler = findViewById(R.id.recycler)
+        val adapter = NotesAdapter()
+        recycler.adapter = adapter // устанавливаю адаптер в ресайклер
+        recycler.layoutManager = LinearLayoutManager(this)
+        adapter.setNotes(fakeNotes)
 
         addNoteButton = findViewById(R.id.add_note_button)
         addNoteButton.setOnClickListener {
@@ -24,6 +34,5 @@ class MainActivity : AppCompatActivity() {
             val intent = Intent(this, NoteCreationActivity::class.java)
             startActivity(intent)
         }
-
     }
 }
